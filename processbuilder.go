9dddb06783540ebe879897df24fc53fc258c6182
Alessandro Crugnola <alessandro.crugnola@swisscom.com>
2023-01-17 01:30:46 +0100

    process

--- processbuilder.go
+++ processbuilder.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"os"
 	"os/exec"
 	"strings"
 	"time"
@@ -21,38 +22,44 @@ type command struct {
 	command string
 	args    []string
 
+	StdOut io.Writer
+	StdErr io.Writer
+	StdIn  io.Reader
+
+	cmd        *exec.Cmd
 	pipeReader *io.PipeReader
 	pipeWriter *io.PipeWriter
-	cmd        *exec.Cmd
 	exitCode   int
 }
 
-func (c command) close() {
-	if c.pipeWriter != nil {
-		c.pipeWriter.Close()
-	}
-	if c.pipeReader != nil {
-		c.pipeReader.Close()
-	}
-}
-
-func (c command) String() string {
-	return fmt.Sprintf("%s %s", c.command, strings.Join(c.args, " "))
-}
-
 type Option struct {
-	timeout    time.Duration
-	stdout     io.Writer
+	Timeout time.Duration
+	Close   *chan os.Signal
+
 	stdoutPipe bool
 }
 
+func EmptyOption() Option {
+	return Option{}
+}
+
 type Processbuilder struct {
-	cmds       []*command
+	cmds   []*command
+	option *Option
+
 	started    bool
 	exited     bool
 	cancelFn   context.CancelFunc
 	Ctx        context.Context
 	StdoutPipe io.ReadCloser
+}
+
+func (p *Processbuilder) Count() int {
+	return len(p.cmds)
+}
+
+func (p *Processbuilder) GetCmd(index int) *exec.Cmd {
+	return p.cmds[index].cmd
 }
 
 func (p *Processbuilder) close() {
@@ -62,19 +69,18 @@ func (p *Processbuilder) close() {
 	}
 }
 
-func (p *Processbuilder) prepare(option Option) (*Processbuilder, error) {
-	if len(p.cmds) == 0 {
+func (p *Processbuilder) prepare() (*Processbuilder, error) {
+	total := len(p.cmds)
+
+	if total == 0 {
 		return nil, ErrNoCommands
 	}
-
-	total := len(p.cmds)
-	fmt.Printf("total commands: %d\n", total)
 
 	var cancel context.CancelFunc
 	var ctx context.Context
 
-	if option.timeout > 0 {
-		ctx, cancel = context.WithTimeout(context.Background(), option.timeout)
+	if p.option.Timeout > 0 {
+		ctx, cancel = context.WithTimeout(context.Background(), p.option.Timeout)
 	} else {
 		ctx, cancel = context.WithCancel(context.Background())
 	}
@@ -82,52 +88,93 @@ func (p *Processbuilder) prepare(option Option) (*Processbuilder, error) {
 	p.Ctx = ctx
 	p.cancelFn = cancel
 
+	var previousCommand *command
+
 	// prepare commands
 	for index, command := range p.cmds {
 		fmt.Printf("%d/%d preparing %s\n", index, total, command.String())
 
 		command.cmd = exec.CommandContext(ctx, command.command, command.args...)
 
-		if index > 0 {
-			command.cmd.Stdin = p.cmds[index-1].pipeReader
+		// checks
+
+		if index != 0 && command.StdIn != nil {
+			return nil, errors.New("stdin allowed only for the first command")
 		}
 
+		if index != total-1 && command.StdOut != nil {
+			return nil, errors.New("stdout allowed only for the last command")
+		}
+
+		if command.StdErr != nil {
+			command.cmd.Stderr = command.StdErr
+		}
+
+		// first command
+		if index == 0 {
+			if command.StdIn != nil {
+				command.cmd.Stdin = command.StdIn
+			}
+		}
+
+		// second .. last
+		if index > 0 {
+			previousCommand = p.cmds[index-1]
+			command.cmd.Stdin = previousCommand.pipeReader
+		}
+
+		// first .. second to last
 		if index < total-1 {
 			pipeReader, pipeWriter := io.Pipe()
+			fmt.Printf("pipereader=%v, pipewriter=%v\n", pipeReader, pipeWriter)
+
 			command.pipeWriter = pipeWriter
 			command.pipeReader = pipeReader
 			command.cmd.Stdout = pipeWriter
 		}
 
+		// last command
 		if index == total-1 {
-			if option.stdoutPipe {
-				pipe, err := command.cmd.StdoutPipe()
-				if err != nil {
-					return nil, err
-				}
-				p.StdoutPipe = pipe
-			} else if option.stdout != nil {
-				command.cmd.Stdout = option.stdout
+			if command.StdOut != nil {
+				command.cmd.Stdout = command.StdOut
 			}
+			// if p.option.stdoutPipe {
+			// 	pipe, err := command.cmd.StdoutPipe()
+			// 	if err != nil {
+			// 		return nil, err
+			// 	}
+			// 	p.StdoutPipe = pipe
+			// } else if p.option.stdout != nil {
+			// 	fmt.Printf("[%d] %s stdout = option.stdout\n", index, command.command)
+			// 	command.cmd.Stdout = p.option.stdout
+			// } else {
+			// 	fmt.Printf("[%d] %s stdout = os.Stdout\n", index, command.command)
+			// 	command.cmd.Stdout = os.Stdout
+			// }
 		}
 	}
 
 	return p, nil
 }
 
-func (p *Processbuilder) output(option Option) ([]byte, int, error) {
+func (p *Processbuilder) output() (*bytes.Buffer, int, error) {
+	var total = p.Count()
 	var outBuffer bytes.Buffer
 
-	option.stdout = &outBuffer
-	result, err := p.prepare(option)
+	if total > 0 {
+		if p.cmds[total-1].StdOut != nil {
+			return nil, -1, errors.New("stdout not allowed on the last command")
+		}
+		p.cmds[total-1].StdOut = &outBuffer
+	}
+
+	_, err := p.prepare()
 
 	if err != nil {
 		return nil, 0, err
 	}
 
-	total := len(p.cmds)
-
-	defer result.cancelFn()
+	defer p.cancelFn()
 	defer p.close()
 
 	if err := Start(p); err != nil {
@@ -135,38 +182,26 @@ func (p *Processbuilder) output(option Option) ([]byte, int, error) {
 	}
 
 	if _, _, err := Wait(p); err != nil {
-		return nil, 0, err
+		return nil, -1, err
 	}
 
-	return outBuffer.Bytes(), p.cmds[total-1].exitCode, nil
+	return &outBuffer, p.cmds[total-1].exitCode, nil
 }
 
-func Pipe(cmd ...*command) (*Processbuilder, error) {
-	return PipeWithTimeout(0, cmd...)
+func Pipe(option Option, cmd ...*command) (*Processbuilder, error) {
+	option.stdoutPipe = true
+	p := &Processbuilder{cmds: cmd, option: &option}
+	return p.prepare()
 }
 
-func PipeWithTimeout(timeout time.Duration, cmd ...*command) (*Processbuilder, error) {
-	p := &Processbuilder{cmds: cmd}
-
-	o := Option{
-		stdoutPipe: true,
-		timeout:    timeout,
-	}
-
-	result, err := p.prepare(o)
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
+func Create(option Option, cmd ...*command) (*Processbuilder, error) {
+	p := &Processbuilder{cmds: cmd, option: &option}
+	return p.prepare()
 }
 
-func Output(cmds ...*command) ([]byte, int, error) {
-	return OutputWithTimeout(0, cmds...)
-}
-
-func OutputWithTimeout(timeout time.Duration, cmds ...*command) ([]byte, int, error) {
-	p := &Processbuilder{cmds: cmds}
-	return p.output(Option{timeout: timeout})
+func Output(option Option, cmds ...*command) (*bytes.Buffer, int, error) {
+	p := &Processbuilder{cmds: cmds, option: &option}
+	return p.output()
 }
 
 func Start(p *Processbuilder) error {
@@ -195,33 +230,43 @@ func Wait(p *Processbuilder) (int, *exec.Cmd, error) {
 
 	defer p.close()
 
+	if p.option.Close != nil {
+		go func() {
+			<-*p.option.Close
+			fmt.Println("Received kill signal!")
+			Kill(p)
+		}()
+	}
+
+	var previousCommand *command
+	var lastCommand = p.cmds[total-1]
+
 	for index, command := range p.cmds {
 		fmt.Printf("%d/%d calling wait on command %s\n", index, total, command.String())
 
-		err := command.cmd.Wait()
-		exitCode := command.cmd.ProcessState.ExitCode()
-
-		if exiterr, ok := err.(*exec.ExitError); ok {
-			exitCode = exiterr.ExitCode()
+		if err := command.cmd.Wait(); err != nil {
+			return command.cmd.ProcessState.ExitCode(), nil, err
 		}
 
+		exitCode := command.cmd.ProcessState.ExitCode()
 		command.exitCode = exitCode
 
 		if command.pipeWriter != nil {
+			fmt.Printf("[%d] closing pipeWripter %v of command %s\n", index, command.pipeReader, command.command)
 			command.pipeWriter.Close()
 		}
 
 		if index > 0 {
-			if p.cmds[index-1].pipeReader != nil {
-				p.cmds[index-1].pipeReader.Close()
+			previousCommand = p.cmds[index-1]
+			if previousCommand.pipeReader != nil {
+				fmt.Printf("[%d] closing pipeReader %v of command %s\n", index, previousCommand.pipeReader, command.command)
+				previousCommand.pipeReader.Close()
 			}
 		}
-
-		if err != nil {
-			return exitCode, nil, err
-		}
 	}
-	return p.cmds[total-1].exitCode, p.cmds[total-1].cmd, nil
+
+	fmt.Printf("exitCode=%d\n", lastCommand.exitCode)
+	return lastCommand.exitCode, lastCommand.cmd, nil
 }
 
 func Kill(p *Processbuilder) error {
@@ -252,4 +297,32 @@ func Command(cmd string, args ...string) *command {
 		command: cmd,
 		args:    args,
 	}
+}
+
+func (c *command) WithStdErr(w io.Writer) *command {
+	c.StdErr = w
+	return c
+}
+
+func (c *command) WithStdOut(w io.Writer) *command {
+	c.StdOut = w
+	return c
+}
+
+func (c *command) WithStdIn(r io.Reader) *command {
+	c.StdIn = r
+	return c
+}
+
+func (c *command) close() {
+	if c.pipeWriter != nil {
+		c.pipeWriter.Close()
+	}
+	if c.pipeReader != nil {
+		c.pipeReader.Close()
+	}
+}
+
+func (c *command) String() string {
+	return fmt.Sprintf("%s %s", c.command, strings.Join(c.args, " "))
 }
--- processbuilder_test.go
+++ processbuilder_test.go
@@ -2,18 +2,24 @@ package processbuilder
 
 import (
 	"bufio"
+	"bytes"
+	"context"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 	"os/signal"
 	"regexp"
 	"syscall"
 	"testing"
+	"time"
 
 	"gotest.tools/v3/assert"
 )
 
 func TestOutput(t *testing.T) {
-	b, code, err := Output(
+	outBuf, code, err := Output(
+		EmptyOption(),
 		Command("ls", "-la"),
 		Command("grep", "process"),
 		Command("sed", "s/process/***/g"),
@@ -23,13 +29,118 @@ func TestOutput(t *testing.T) {
 	assert.Equal(t, 0, code)
 
 	fmt.Println("\nRESULT: ")
+	fmt.Println(outBuf)
+}
 
-	s := string(b)
-	fmt.Println(s)
+func TestScreenRecord(t *testing.T) {
+	r, w := io.Pipe()
+	var errBuf bytes.Buffer
+	// var outBuf bytes.Buffer
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cmd1 := exec.CommandContext(ctx, "adb", "shell", "while true; do screenrecord --output-format=h264 -; done")
+	cmd1.Stdout = w
+	cmd1.Stderr = &errBuf
+
+	cmd2 := exec.CommandContext(ctx, "ffplay", "-framerate", "60", "-probesize", "32", "-sync", "video", "-")
+	cmd2.Stdin = r
+	cmd2.Stdout = os.Stdout
+
+	if err := cmd1.Start(); err != nil {
+		fmt.Println("Error during strt of cmd1")
+		fmt.Println(err)
+		return
+	}
+
+	if err := cmd2.Start(); err != nil {
+		fmt.Println("Error during start of cmd2")
+		fmt.Println(err)
+	}
+
+	if err := cmd1.Wait(); err != nil {
+		fmt.Println("Error during wait of cmd1")
+		fmt.Println(err)
+
+		fmt.Println(errBuf.String())
+	}
+
+	if err := cmd2.Wait(); err != nil {
+		w.Close()
+		r.Close()
+		fmt.Println("Error during wait of cmd2")
+		fmt.Println(err)
+	}
+
+}
+
+func TestAgain(t *testing.T) {
+	closeSignal := make(chan os.Signal)
+	signal.Notify(closeSignal, os.Interrupt, syscall.SIGTERM)
+
+	p, err := Create(
+		Option{Timeout: 30 * time.Second, Close: &closeSignal},
+		Command("adb", "shell", "while true; do screenrecord --output-format=h264 --size=1024x768 -; done"),
+		Command("ffplay", "-framerate", "60", "-probesize", "64", "-sync", "video", "-").WithStdErr(os.Stderr).WithStdOut(os.Stdout),
+	)
+	assert.NilError(t, err)
+
+	err = Start(p)
+	assert.NilError(t, err)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	exitCode, _, err := Wait(p)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	assert.NilError(t, err)
+	assert.Equal(t, 0, exitCode)
+}
+
+func TestCancel(t *testing.T) {
+
+	p, err := Create(
+		Option{Timeout: 30 * time.Second},
+		Command("adb", "shell", "while true; do screenrecord --output-format=h264 -; done"),
+		Command("ffplay", "-framerate", "60", "-probesize", "32", "-sync", "video", "-"),
+	)
+	assert.NilError(t, err)
+
+	err = Start(p)
+	assert.NilError(t, err)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-c
+		fmt.Println("Kill Process!")
+		Kill(p)
+	}()
+
+	exitCode, _, err := Wait(p)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	assert.NilError(t, err)
+	assert.Equal(t, 0, exitCode)
 }
 
 func TestOutputPipe(t *testing.T) {
 	p, err := Pipe(
+		Option{},
 		Command("adb", "logcat"),
 		Command("grep", "Unix"),
 	)
